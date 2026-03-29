ef21b692ab8a7ca2cc04a6e4cb7aec42fa6667fc
Nguyen Trong Tin <s4021012@rmit.edu.vn>
2026-03-29 14:36:37 +0700

    Delete db.php

--- db.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Content-Type: application/json');
-http_response_code(410);
-echo json_encode([
-    "error" => "Legacy PHP database config removed for static GitHub Pages deployment.",
-    "status" => "deprecated"
-]);
-exit;
-?>
