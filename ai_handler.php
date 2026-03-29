9dc2b2218397242e2a4eae00ecc9d42085c67d20
Nguyen Trong Tin <s4021012@rmit.edu.vn>
2026-03-29 14:37:01 +0700

    Delete ai_handler.php

--- ai_handler.php
+++
@@ -1,8 +0,0 @@
-<?php
-header('Content-Type: application/json');
-http_response_code(410);
-echo json_encode([
-    "error" => "Legacy PHP endpoint. GitHub Pages does not execute PHP.",
-    "status" => "deprecated"
-]);
-?>
